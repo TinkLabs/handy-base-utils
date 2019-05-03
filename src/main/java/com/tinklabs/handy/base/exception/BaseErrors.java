8820e8094450b2ab96feecc8a16ef8a0300f5bac
daveyan <dave.yan@tinklabs.com>
2019-05-03 18:25:25 +0800

    添加ResultUtil工具类:判断返回结果是否为空

--- src/main/java/com/tinklabs/handy/base/exception/BaseErrors.java
+++ src/main/java/com/tinklabs/handy/base/exception/BaseErrors.java
@@ -1,19 +1,19 @@
 package com.tinklabs.handy.base.exception;
 
 public enum BaseErrors implements IError {
-	
+
 	SUCCESS("0","success"),
 	PARAMS_EMPTY("100001","parameter(s) can not be empty."),
 	UNKNOW_EXCEPTION("100002","unknowed exception."),
 	BUSINESS_EXCEPTION("10003", "business exception"),
 	SYSTEM_EXCEPTION("500","server internal error."),
+	BAECODE_IS_NOT_EXIST("20000","barcode is not exist."),
 	AUTHENTICATION_EXCEPTION("403","no auth.");
-    
-	
+
 	private String code;
-	
+
 	private String msg;
-	
+
 	BaseErrors(String code,String msg) {
 		this.code = code;
 		this.msg = msg;
